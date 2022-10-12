5f0a102cd87368b1082a6553c2053478237cc728
Paweł Marks <pmarks@virtuslab.com>
2022-10-12 14:59:42 +0200

    Add local elimination test

--- test/BasicSuite.scala
+++ test/BasicSuite.scala
@@ -11,20 +11,31 @@ class BasicSuite extends munit.FunSuite:
     val work = task:
       obtainB + obtainA
 
-    val res1 =
+    val res =
       given A = A(5)
       given B = B("test")
 
       work.run
-    assertEquals(res1, "test5")
+    assertEquals(res, "test5")
 
   test("two context elements, one missing, not compiling"):
-    val work = task {
+    val work = task:
       obtainB + obtainA
-    }
 
     given A = A(5)
     val errors = clue(typeCheckErrors("work.run"))
 
     assertEquals(errors.size, 1)
     assertEquals(errors.head.message, "No given instance of type test.utils.B was found for parameter of (test.utils.A, test.utils.B) ?=> String")
+
+  test("part of context can be locally eliminated"):
+    val work = task:
+      given A = A(7)
+      obtainB + obtainA
+
+    val res =
+      given A = A(5)
+      given B = B("test")
+      work.run
+
+    assertEquals(res, "test7")
